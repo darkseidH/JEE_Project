0dfc6059ff1452c67a9bba6c0561276868d48401
said- <said.elhafyani@usmba.ac.am>
2023-11-25 20:32:16 +0100

    Second commit

--- src/main/java/com/buisness/I_Gestion_User.java
+++ src/main/java/com/buisness/I_Gestion_User.java
@@ -2,7 +2,7 @@ package com.buisness;
 
 import com.presentation.model.User;
 
-public interface Interface_Gestion_User {
+interface Interface_Gestion_User {
     public User addUSer(User user);
     public boolean deleteUser(User user);
     public User updateUser(User user);
